bfd1e5eff2a35f417d653ddf47ef2cb067c24fcd
jgre579 <jgre579@aucklanduni.ac.nz>
2022-05-18 16:19:15 +1200

    Add category cards click listener

--- app/src/main/java/com/example/j_tech/MainActivity.java
+++ app/src/main/java/com/example/j_tech/MainActivity.java
@@ -5,10 +5,44 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.card.MaterialCardView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    MainViewHolder vh;
+
+    class MainViewHolder {
+
+        TextView devicesTextView;
+
+        public MainViewHolder(){
+
+            devicesTextView = findViewById(R.id.devices_text_view);
+
+        }
+
+        public String getCategoryText(View view) {
+
+            MaterialCardView cardView = (MaterialCardView) view;
+            LinearLayout layout = (LinearLayout) cardView.getChildAt(0);
+            TextView textView = (TextView) layout.getChildAt(0);
+
+            return (String) textView.getText();
+
+        }
+
+
+
+
+    }
+
 
     ArrayList<Device> topPicks;
 
@@ -16,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vh = new MainViewHolder();
 
         RecyclerView topPicksRV = (RecyclerView) findViewById(R.id.top_picks_recycler_view);
 
@@ -29,4 +65,13 @@ public class MainActivity extends AppCompatActivity {
         topPicksRV.setLayoutManager(layoutManager);
 
     }
+
+    public void clickCategory(View view) {
+
+        vh.devicesTextView.setText(vh.getCategoryText(view));
+
+
+    }
+
+
 }
